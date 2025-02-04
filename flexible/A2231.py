db4b8538134120d68dae1fb5d387b8a10c2993a6
HeeSeok <mungml1995@gmail.com>
2025-02-05 02:26:09 +0900

    250205

--- flexible/A2231.py
+++ flexible/A2231.py
@@ -1,11 +1,9 @@
 n = int(input())
 
 min_v = 0
-start = max(int(n/100), 0)
+start = max(1, n - (len(str(n)) * 9)) # 999의 경우를 생각해보자 각 자리수의 합은 (자리수*9)를 넘을 수 없다.
+
 for i in range(start, n):
-
-    jSum = 0
-
     j_list = list(map(int, str(i)))
     jSum = sum(j_list)
     if n == (i + jSum):
