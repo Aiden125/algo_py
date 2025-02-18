6f0766399af448342b85e594624386e35f27ddea
HeeSeok <mungml1995@gmail.com>
2025-02-18 23:38:41 +0900

    250218

---
+++ simulation/A14719.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+arr =  list(map(int, input().split()))
+
+left_index = 0
+right_index = 1
+
+min_v = 0
+total = 0
+sum_list = []
+while right_index < len(arr):
+    left = arr[left_index]
+    right = arr[right_index]
+
+    if left > right:
+        sum_list.append(arr[right_index])
+        right_index += 1
+    elif (left <= right) or (right_index == len(arr) - 1):
+        min_v = min(left, right)
+        for i in sum_list:
+            total += min_v - i
+        sum_list = []
+        left_index = right_index
+        right_index += 1
+print(total)
