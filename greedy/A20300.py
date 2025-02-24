a6c78e41cbdbaf5cba658ae8a40fe80cd25a27da
HeeSeok <mungml1995@gmail.com>
2025-02-25 01:35:35 +0900

    250225

--- greedy/A20300.py
+++ greedy/A20300.py
@@ -6,10 +6,14 @@ arr.sort()
 left = 0
 right = len(arr)-1
 
-maxV = -1
+if len(arr) % 2 == 0:
+    maxV = -1
+else:
+    right = len(arr) - 2
+    maxV = arr[len(arr) - 1]
 while left <= right:
-    if left != right:
-        maxV = max(arr[left] + arr[right], maxV)
-    left += 1
-    right -= 1
+        if left != right:
+            maxV = max(arr[left] + arr[right], maxV)
+        left += 1
+        right -= 1
 print(maxV)
