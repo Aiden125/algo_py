df67b5b89113dafdeabf6d892a3f4757a27aae44
DESKTOP-KVGJO43\mungm <mungml1995@gmail.com>
2025-05-01 00:06:36 +0900

    250501

--- dfs/A2606.py
+++ dfs/A2606.py
@@ -7,9 +7,12 @@ def dfs(arr, number, visited):
 
 n = int(input())
 m = int(input())
-graph = [] * (n + 1)
+graph = [[] for _ in range(n+1)] 
 visited = [False] * (n + 1)
 for _ in range(m):
     a, b = list(map(int, input().split()))
-    graph(a).append(b)
-    graph(b).append(a)
+    graph[a].append(b)
+    graph[b].append(a)
+
+dfs(graph, 1, visited)
+print(visited.count(True) - 1)
